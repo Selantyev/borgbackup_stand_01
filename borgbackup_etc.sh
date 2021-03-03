28a50477c2d5dc56bda61071e0106480b927e734
Selantyev <73749919+Selantyev@users.noreply.github.com>
2021-03-03 19:00:12 +0600

    Create borgbackup_etc.sh

---
+++ borgbackup_etc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Borgbackup script
+
+# User and server name
+BACKUP_USER=root
+BACKUP_SERVER=backup-server
+
+# Backup type (data, etc, mysql, system)
+BACKUP_TYPE=etc
+
+# Remote repository
+REPOSITORY=$BACKUP_USER@$BACKUP_SERVER:/var/backups/$(hostname)-${BACKUP_TYPE}
+
+# Export passphrase
+export BORG_PASSCOMMAND="cat $HOME/.borg-passphrase"
+
+# Create backup
+borg create -v -s $REPOSITORY::'{now:%Y-%m-%d-%H-%M}' /etc
+
+# Prune backups
+borg prune -v --show-rc --list $REPOSITORY \
+           --keep-within=92d --keep-monthly=-1
